526662215ad92780f18f09676081a0d66914f5d4
Kalilamodow <kalilamodow@gmail.com>
2026-09-10 21:04:03 -0500

    feat: linux font loading! now just to fix storage

--- src/core/fonts.rs
+++ src/core/fonts.rs
@@ -1,14 +1,8 @@
 use eframe::egui;
+use egui::{FontData, FontDefinitions};
+use std::fs;
 
-#[cfg(windows)]
-pub fn load_fonts(ctx: &egui::Context) -> Result<(), Box<dyn std::error::Error>> {
-    use egui::{FontData, FontDefinitions};
-    use std::fs;
-    const FONT_PATH: &str = "C:\\Windows\\Fonts\\segoeui.ttf";
-
-    let file_content = fs::read(FONT_PATH)?;
-    let font_data = FontData::from_owned(file_content);
-
+fn insert_font_data(ctx: &egui::Context, font_data: FontData) {
     let mut fonts = FontDefinitions::default();
     fonts
         .font_data
@@ -21,10 +15,44 @@ pub fn load_fonts(ctx: &egui::Context) -> Result<(), Box<dyn std::error::Error>>
         .insert(0, "Segoe UI".to_owned());
 
     ctx.set_fonts(fonts);
+}
+
+#[cfg(windows)]
+pub fn load_fonts(ctx: &egui::Context) -> Result<(), Box<dyn std::error::Error>> {
+    const FONT_PATH: &str = "C:\\Windows\\Fonts\\segoeui.ttf";
+
+    let file_content = fs::read(FONT_PATH)?;
+    let font_data = FontData::from_owned(file_content);
+    insert_font_data(ctx, font_data);
+
     Ok(())
 }
 
 #[cfg(not(windows))]
-pub fn load_fonts(_ctx: &egui::Context) -> Result<(), Box<dyn std::error::Error>> {
+pub fn load_fonts(ctx: &egui::Context) -> Result<(), Box<dyn std::error::Error>> {
+    let mut database = fontdb::Database::new();
+    database.load_system_fonts();
+
+    let font_id = database
+        .query(&fontdb::Query {
+            families: &[
+                fontdb::Family::Name("DejaVu Sans"),
+                fontdb::Family::Name("Noto Sans"),
+                fontdb::Family::Name("Liberation Sans"),
+            ],
+            ..fontdb::Query::default()
+        })
+        .ok_or("no sans serif font")?;
+    let face = database.face(font_id).ok_or("whered it go")?;
+
+    let path = match &face.source {
+        fontdb::Source::File(path) => path,
+        _ => return Err("theres no file for the font help".into()),
+    };
+
+    let file_content = fs::read(path)?;
+    let font_data = FontData::from_owned(file_content);
+    insert_font_data(ctx, font_data);
+
     Ok(())
 }
